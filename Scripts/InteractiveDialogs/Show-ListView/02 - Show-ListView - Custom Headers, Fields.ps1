a4139985cae9c312431d441dc089bca3737b4a71
Brandon Bruno <bmbruno@gmail.com>
2020-07-23 09:37:14 -0400

    Moved Properties into props object.

--- Scripts/InteractiveDialogs/Show-ListView/02 - Show-ListView - Custom Headers, Fields.ps1
+++ Scripts/InteractiveDialogs/Show-ListView/02 - Show-ListView - Custom Headers, Fields.ps1
@@ -1,9 +1,15 @@
 # Datasource
 $items = Get-ChildItem -Path 'master:/sitecore/content/demosite'
 
-# Show list view dialog with custom headers and values
-$items | Show-ListView -Property `
+# Configure columns and data mapping
+$props = @(     
     @{ Name = "Name"; Expression = { $_.Name } },
     @{ Name = "Display Name"; Expression = { $_.DisplayName } },
     @{ Name = "Full Path"; Expression = { $_.Paths.FullPath } }
-    @{ Name = "Path Length"; Expression = { $_.Paths.FullPath.Length } }
+    @{ Name = "Path Length"; Expression = { $_.Paths.FullPath.Length } } 
+)
+
+# Show list view dialog with custom headers and values
+$items | Show-ListView `
+    -InfoDescription 'This is an informational message' `
+    -Property $props
