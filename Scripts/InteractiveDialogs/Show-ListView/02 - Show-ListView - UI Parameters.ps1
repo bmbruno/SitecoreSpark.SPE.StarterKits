9982d173719399aaaf72dc40d997ad9e2b96d6fd
Brandon Bruno <bmbruno@gmail.com>
2020-07-24 08:51:23 -0400

    Added UI Parameters example.

--- Scripts/InteractiveDialogs/Show-ListView/02 - Show-ListView - UI Parameters.ps1
+++ Scripts/InteractiveDialogs/Show-ListView/02 - Show-ListView - UI Parameters.ps1
@@ -9,6 +9,13 @@ $props = @(
     @{ Name = "Path Length"; Expression = { $_.Paths.FullPath.Length } } 
 )
 
-# Show list view dialog with custom headers and values
+# Show list view dialog with custom UI elements defined:
+
+# InfoTitle / InfoDescription: displays a custom informational message near the top of the window
+# MissingDataMessage: message displayed when no results are returned
+
 $items | Show-ListView `
-    -Property $props
+    -Property $props `
+    -InfoTitle 'Example Info Title' `
+    -InfoDescription 'Lorem ipsum dolor sit amet.' `
+    -MissingDataMessage 'There are no results to display.'
