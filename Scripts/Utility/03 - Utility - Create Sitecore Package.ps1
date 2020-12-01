9e9a549bcd155664e14f4808c9a226df3936f53f
Brandon Bruno <bmbruno@gmail.com>
2020-11-30 19:41:35 -0500

    Added Sitecore package example.

--- Scripts/Utility/03 - Utility - Create Sitecore Package.ps1
+++ Scripts/Utility/03 - Utility - Create Sitecore Package.ps1
@@ -1,28 +1,22 @@
-# Choose one of these methods for loading items to the package:
-#   1) by path
-#   2) by template name
-#   3) by template ID
-#   ... or write your own logic to populate '$items'
+# Load items to include in the package; use Where-Object to filter down select
+# A couple of Where-Object clauses are provided below
 
-$items = Get-ChildItem -Path 'master:/sitecore/content/mysite' -Recurse
-# $items = Get-ChildItem -Path 'master:/sitecore' -Recurse | Where-Object { $_.TemplateName -eq 'TEMPLATE_NAME' }
-# $items = Get-ChildItem -Path 'master:/sitecore' -Recurse | Where-Object { $_.TemplateId -eq '{guid}' }
+$items = Get-ChildItem -Path 'master:/sitecore/content' -Recurse
+# | Where-Object { $_.TemplateName -eq 'TEMPLATE_NAME' }
+# | Where-Object { $_.TemplateId -eq '{guid}' }
 
-#
+Write-Host "Found $($items.Count) items for package."
+
 # Create (and configure) package and related metadata
-#
-
 $package = New-Package -Name 'PACKAGE_NAME'
 $package.Metadata.Author = 'PACKAGE_AUTHOR'
 $package.Metadata.Version = '1'
 $package.Metadata.Readme = 'README_TEXT'
 
 # Create a source for the items
-$source = $items | New-ItemSource -Name 'PACKAGE_SOURCE_NAME' -InstallMode Overwrite
-$package.Sources.Add($source);
+$source = $items | New-ExplicitItemSource -Name 'PACKAGE_SOURCE_NAME' -InstallMode Overwrite
+$package.Sources.Add($source)
 
 # Create, export, and download the package
-$packageFolder = '\App_Data\packages\'
-
 Export-Package -Project $package -Path "$($package.Name).zip" -Zip
-Download-File "$packageFolder\$($package.Name).zip"
+Download-File "$SitecorePackageFolder\$($package.Name).zip"
